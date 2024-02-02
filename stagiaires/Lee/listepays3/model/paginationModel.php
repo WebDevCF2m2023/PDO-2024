eb4d8ef7eb8f4c16515e9c3595cd6e9aa1f07b85
leerlandais <leerlandais@gmail.com>
2024-02-02 09:43:52 +0100

    page links

--- stagiaires/Lee/listepays3/model/paginationModel.php
+++ stagiaires/Lee/listepays3/model/paginationModel.php
@@ -9,10 +9,10 @@ function PaginationModel(string $URL, string $getName, int $nbTotalItem, int $cu
 
     return (string) $nbPage;
 
-}
+    }
 
-$page = PaginationModel("http://pdo-2024/stagiaires/Lee/listepays3/model/paginationModel.php", MY_PAGINATION_GET, 1340, 2, MY_PAGINATION_BY_PAGE);
+
+$page = PaginationModel("http://pdo-2024/stagiaires/Lee/listepays3/model/paginationModel.php", MY_PAGINATION_GET, 150, 2, MY_PAGINATION_BY_PAGE);
 
 var_dump($page);
-
-
+  ?>
--- stagiaires/Lee/listepays3/view/homepage.view.php
+++ stagiaires/Lee/listepays3/view/homepage.view.php
@@ -10,12 +10,21 @@
     <h2>Liste de tous les pays du monde</h2>
     <h3>Nombre de pays : <?=$countCountries?> </h3>
     <?php 
+        require_once "../model/paginationModel.php";
+        $i=1;
+    do{
+      ?>
+    <a href ="?pg=<?=$i?>"><?=$i?></a>
+    <?php
+        $i++;
+    }while($i <= $page);
+    echo "</br>";
+
     foreach($allCountries as $pays):       
     ?>
     <p><?=$pays['id'].': '.$pays['nom']?></p> 
     <?php
     endforeach;
-    
     ?>
 </body>
 </html>
