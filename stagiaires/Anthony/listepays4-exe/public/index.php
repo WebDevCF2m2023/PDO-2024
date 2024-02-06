43f9e907827c4cfdfebf8517dfdc3bb665700a4c
Anthoonyyy <Anthony212007@hotmail.com>
2024-02-06 16:17:11 +0100

    listepays4

---
+++ stagiaires/Anthony/listepays4-exe/public/index.php
@@ -0,0 +1,42 @@
+<?php
+/*
+** Contrôleur frontal
+*/
+
+// chargement des dépendances
+require_once "../config.php"; // constantes
+require_once "../model/CountriesModel.php"; // fonctions lié à la table countries
+require_once "../model/PaginationModel.php"; // fonction de pagination 
+
+// tentative de connexion
+try{
+    // création d'une instance de PDO 
+    $db = new PDO(MY_DB_TYPE.":host=".MY_DB_HOST.";port=".MY_DB_PORT.";dbname=". MY_DB_NAME.";charset=".MY_DB_CHARSET, MY_DB_LOGIN, MY_DB_PWD);
+    
+    // on va mettre la valeur par défaut du format des fetch (la conversion des données récupérées en PHP) en tableau associatif, en utilisant PDO::FETCH_ASSOC
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+// problème lors de la connexion, utilisation de la classe Exception    
+}catch(Exception $e){ 
+
+    // arrêt du script et affichage de l'erreur
+    die("Erreur : ".$e->getMessage());
+}
+//on récupère le nombre total de pays
+$nbPays =getNumberCountries($db);
+
+$pagination =// PaginationModel("./");
+
+// requête sur la DB (se trouve dans le dossier model car gestion de données)
+//a remplacer par getcountriesbypage
+$allCountries = getCountriesByPage($db);   //remplacement par getcountriesbypage
+
+
+/* récupération du template d'affichage, 
+on utilisera la boucle while avec un fetch directement
+dans la vue */
+
+include "../view/homepage.view.php";
+
+// déconnexion (bonne pratique)
+$db=null;
