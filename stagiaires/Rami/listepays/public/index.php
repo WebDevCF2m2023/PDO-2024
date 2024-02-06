279dfa94743034aab1875441b6556182b91d57af
rammroumm2K <ramybrz00@gmail.com>
2024-02-06 14:19:23 +0100

    OuO

--- stagiaires/Rami/listepays/public/index.php
+++ stagiaires/Rami/listepays/public/index.php
@@ -1,35 +1,36 @@
 <?php
 /*
-Contrôleur frontal 
+** Contrôleur frontal
 */
 
-//chargement des dépandances 
-
+// chargement des dépendances
 require_once "../config.php";
 
-//tentative de connexion
-
+// tentative de connexion
 try{
-    $db= new PDO(
-MY_DB_TYPE.":host=".MY_DB_HOST.";port=".MY_DB_PORT.";dbname=". MY_DB_NAME.";charset=".MY_DB_CHARSET,
-MY_DB_LOGIN,MY_DB_PWD
-    );
-
+    // création d'une instance de PDO 
+    $db = new PDO(MY_DB_TYPE.":host=".MY_DB_HOST.";port=".MY_DB_PORT.";dbname=". MY_DB_NAME.";charset=".MY_DB_CHARSET, MY_DB_LOGIN, MY_DB_PWD);
 }catch(Exception $e){
+    // arrêt du script et affichage de l'erreur
     die("Erreur : ".$e->getMessage());
 }
 
-//requéte sur la DB (model car gestion de données)
+// requête sur la DB (model car gestion de données)
 
-$sql="SELECT * FROM countries";
+$sql = "SELECT * FROM countries"; // requête non exécutée
 
-$query = $db->query($sql);
+$query = $db->query($sql); // exécution de la requête de type SELECT avec query()
 
-$countQuery= $query->rowCount();//nombre de résultat de notre réquete 
+$countQuery = $query->rowCount(); // nombre de résultats de notre requête
 
-//récupzeration du template d'affichage, on utilisera la boucle while avec un fetch directmen dans la vue
+/* récupération du template d'affichage, 
+on utilisera la boucle while avec un fetch directement
+dans la vue */
 
 include "../view/homepage.view.php";
 
-//deconnexion bonne pratique 
+// bonne pratique (autres DB que MySQL ou MariaDB)
+$query->closeCursor();
+
+// déconnexion (bonne pratique)
 $db=null;
